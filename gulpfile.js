7149c3dc05bdc3d351127c0c22ba364be26f55e9
Ahsan Mirza <ahsan186@hotmail.co.uk>
2017-11-20 22:15:58 +0000

    Added all files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/scss/base/*.scss'], ['sass']);
+    gulp.watch(['src/scss/layout/*.scss'], ['sass']);
+    gulp.watch(['src/scss/modules/*.scss'], ['sass']);
+    // gulp.watch(['src/scss/_variables.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
